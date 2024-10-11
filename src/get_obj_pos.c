8ec00c964d5b069e961af590eed9edf61b922e43
Jose Garcia Manzanaro <jgarcia3@c1r8s6.42madrid.com>
2024-10-11 16:16:02 +0200

    Revert "voy a volver atras con revert"
    This reverts commit cbb306355e5793da6dcf37affa158891fd9e7f8b.

--- src/get_obj_pos.c
+++ src/get_obj_pos.c
@@ -6,25 +6,11 @@
 /*   By: jgarcia3 <jgarcia3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 19:37:29 by jgarcia3          #+#    #+#             */
-/*   Updated: 2024/10/11 15:58:37 by jgarcia3         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:44:02 by jgarcia3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-
-/*for debugging */
-void	print_map(char **map)
-{
-	int i;
-
-	i = 0;
-	while (map[i] != NULL)
-	{
-		write(1, map[i], ft_strlen(map[i]));
-		i++;
-	}
-	write(1, "\n", 1);
-}
 
 /* imputs:
 	-map
@@ -37,29 +23,27 @@ int get_obj_pos(char **map, char obj, char xy)
 {
 	int		y;
 	int 	x;
+	char	**map_dup;
 
+	map_dup = map;
 	y = 0;
 	x = 0;
-	print_map(map);
-	while (map[y] != NULL && (xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E'))
+	while (map_dup[y] != NULL && (xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E'))
 	{
-		//printf("-----get_obj_pos----");
-		while (map[y][x] != '\0')
+		printf("-----get_obj_pos----");
+		while (map_dup[y][x] != '\0')
 		{
-			if (map[y][x] == obj && xy == 'y')
-				return (y);
-			if (map[y][x] == obj && xy == 'x')
+			if (map_dup[y][x] == obj && xy == 'y')
+				return (map_dup - map);
+			if (map_dup[y][x] == obj && xy == 'x')
 				return (x);
-			//if (map[x][y] != NULL)
-			//	printf("x: %d\ny: %d\n simbolo: %c\n", x, y, map[x][y]);
+			printf("x: %d\n y: %d\n", x, y);
 			x++;
 		}
 		x = 0;
-		y++;
-		printf("-----YYYYYYYY----");
-
+		map_dup++;
 	}
-	if (map[y] == NULL)
+	if (map_dup[y] == NULL)
 		ft_putstr_fd("get_obj_pos -> map_dup[y] == NULL", 2);
 	if (!((xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E')))
 		ft_putstr_fd("get_obj_pos -> xy or P, E", 2);
