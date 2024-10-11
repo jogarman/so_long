cbb306355e5793da6dcf37affa158891fd9e7f8b
Jose Garcia Manzanaro <jgarcia3@c1r8s6.42madrid.com>
2024-10-11 16:15:51 +0200

    voy a volver atras con revert

--- src/get_obj_pos.c
+++ src/get_obj_pos.c
@@ -6,11 +6,25 @@
 /*   By: jgarcia3 <jgarcia3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 19:37:29 by jgarcia3          #+#    #+#             */
-/*   Updated: 2024/10/11 15:44:02 by jgarcia3         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:58:37 by jgarcia3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+/*for debugging */
+void	print_map(char **map)
+{
+	int i;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		write(1, map[i], ft_strlen(map[i]));
+		i++;
+	}
+	write(1, "\n", 1);
+}
 
 /* imputs:
 	-map
@@ -23,27 +37,29 @@ int get_obj_pos(char **map, char obj, char xy)
 {
 	int		y;
 	int 	x;
-	char	**map_dup;
 
-	map_dup = map;
 	y = 0;
 	x = 0;
-	while (map_dup[y] != NULL && (xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E'))
+	print_map(map);
+	while (map[y] != NULL && (xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E'))
 	{
-		printf("-----get_obj_pos----");
-		while (map_dup[y][x] != '\0')
+		//printf("-----get_obj_pos----");
+		while (map[y][x] != '\0')
 		{
-			if (map_dup[y][x] == obj && xy == 'y')
-				return (map_dup - map);
-			if (map_dup[y][x] == obj && xy == 'x')
+			if (map[y][x] == obj && xy == 'y')
+				return (y);
+			if (map[y][x] == obj && xy == 'x')
 				return (x);
-			printf("x: %d\n y: %d\n", x, y);
+			//if (map[x][y] != NULL)
+			//	printf("x: %d\ny: %d\n simbolo: %c\n", x, y, map[x][y]);
 			x++;
 		}
 		x = 0;
-		map_dup++;
+		y++;
+		printf("-----YYYYYYYY----");
+
 	}
-	if (map_dup[y] == NULL)
+	if (map[y] == NULL)
 		ft_putstr_fd("get_obj_pos -> map_dup[y] == NULL", 2);
 	if (!((xy == 'y' || xy == 'x') && (obj == 'P' || obj == 'E')))
 		ft_putstr_fd("get_obj_pos -> xy or P, E", 2);
